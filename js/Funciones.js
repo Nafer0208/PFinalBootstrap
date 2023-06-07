861f125ef93be12e324136227947b98abffb1ad4
Nafer0208 <115758460+Nafer0208@users.noreply.github.com>
2023-06-07 16:45:28 -0600

    Add files via upload

--- js/Funciones.js
+++ js/Funciones.js
@@ -1,130 +1,69 @@
-function OpenIMGA()
+function AbrirModal(id)
 {
-    document.getElementById("ExtraInfoCA").innerHTML = "<p>Pan echo con productos 100% organicos, lis para cualquier uso que guste darle.</p><h5>Precio unitario:</h5><p>$0.15</p><h5>Precio al por mayor:</h5><p>$0.10</p>";
-    document.getElementById("ExtraInfoCB").innerHTML = "";
-    document.getElementById("ExtraInfoCC").innerHTML = "";
-    document.getElementById("ExtraInfoCD").innerHTML = "";
-    document.getElementById("ExtraInfoCE").innerHTML = "";
-    document.getElementById("ExtraInfoCF").innerHTML = "";
-    document.getElementById("ExtraInfoCG").innerHTML = "";
-    document.getElementById("ExtraInfoCH").innerHTML = "";
-    document.getElementById("ExtraInfoCI").innerHTML = "";
-    document.getElementById("ExtraInfoCJ").innerHTML = "";
+    $(id).modal("show");console.log("Doble click");
 }
-function OpenIMGB()
-{
-    document.getElementById("ExtraInfoCB").innerHTML = "<p>Muffin echo con ingredientes frescos, relleno de trozos de banana.</p><h5>Precio unitario:</h5><p>$0.25</p><h5>Precio al por mayor:</h5><p>$0.15</p>";
-    document.getElementById("ExtraInfoCA").innerHTML = "";
-    document.getElementById("ExtraInfoCC").innerHTML = "";
-    document.getElementById("ExtraInfoCD").innerHTML = "";
-    document.getElementById("ExtraInfoCE").innerHTML = "";
-    document.getElementById("ExtraInfoCF").innerHTML = "";
-    document.getElementById("ExtraInfoCG").innerHTML = "";
-    document.getElementById("ExtraInfoCH").innerHTML = "";
-    document.getElementById("ExtraInfoCI").innerHTML = "";
-    document.getElementById("ExtraInfoCJ").innerHTML = "";
-}
-function OpenIMGC()
-{
-    document.getElementById("ExtraInfoCC").innerHTML = "<p>Pan de concha, con un interior suave y un exterior crujiente, perfecto para un cafe por la tarde.</p><h5>Precio unitario:</h5><p>$0.15</p><h5>Precio al por mayor:</h5><p>$0.10</p>";
-    document.getElementById("ExtraInfoCA").innerHTML = "";
-    document.getElementById("ExtraInfoCB").innerHTML = "";
-    document.getElementById("ExtraInfoCD").innerHTML = "";
-    document.getElementById("ExtraInfoCE").innerHTML = "";
-    document.getElementById("ExtraInfoCF").innerHTML = "";
-    document.getElementById("ExtraInfoCG").innerHTML = "";
-    document.getElementById("ExtraInfoCH").innerHTML = "";
-    document.getElementById("ExtraInfoCI").innerHTML = "";
-    document.getElementById("ExtraInfoCJ").innerHTML = "";
-}
-function OpenIMGD()
-{
-    document.getElementById("ExtraInfoCD").innerHTML = "<p>Pan crujiente y perfecto para cualquier ocacion, se puede acompañar con .</p><h5>Precio unitario:</h5><p>$0.25</p><h5>Precio al por mayor:</h5><p>$0.15</p>";
-    document.getElementById("ExtraInfoCA").innerHTML = "";
-    document.getElementById("ExtraInfoCB").innerHTML = "";
-    document.getElementById("ExtraInfoCC").innerHTML = "";
-    document.getElementById("ExtraInfoCE").innerHTML = "";
-    document.getElementById("ExtraInfoCF").innerHTML = "";
-    document.getElementById("ExtraInfoCG").innerHTML = "";
-    document.getElementById("ExtraInfoCH").innerHTML = "";
-    document.getElementById("ExtraInfoCI").innerHTML = "";
-    document.getElementById("ExtraInfoCJ").innerHTML = "";
-}
-function OpenIMGE()
-{
-    document.getElementById("ExtraInfoCE").innerHTML = "<p>Pan echo con productos 100% organicos.</p><h5>Precio unitario:</h5><p>$0.25</p><h5>Precio al por mayor:</h5><p>$0.15</p>";
-    document.getElementById("ExtraInfoCA").innerHTML = "";
-    document.getElementById("ExtraInfoCB").innerHTML = "";
-    document.getElementById("ExtraInfoCC").innerHTML = "";
-    document.getElementById("ExtraInfoCD").innerHTML = "";
-    document.getElementById("ExtraInfoCF").innerHTML = "";
-    document.getElementById("ExtraInfoCG").innerHTML = "";
-    document.getElementById("ExtraInfoCH").innerHTML = "";
-    document.getElementById("ExtraInfoCI").innerHTML = "";
-    document.getElementById("ExtraInfoCJ").innerHTML = "";
-}
-function OpenIMGF()
-{
-    document.getElementById("ExtraInfoCF").innerHTML = "<p>Pan echo con productos 100% organicos.</p><h5>Precio unitario:</h5><p>$0.25</p><h5>Precio al por mayor:</h5><p>$0.15</p>";
-    document.getElementById("ExtraInfoCA").innerHTML = "";
-    document.getElementById("ExtraInfoCB").innerHTML = "";
-    document.getElementById("ExtraInfoCC").innerHTML = "";
-    document.getElementById("ExtraInfoCD").innerHTML = "";
-    document.getElementById("ExtraInfoCE").innerHTML = "";
-    document.getElementById("ExtraInfoCG").innerHTML = "";
-    document.getElementById("ExtraInfoCH").innerHTML = "";
-    document.getElementById("ExtraInfoCI").innerHTML = "";
-    document.getElementById("ExtraInfoCJ").innerHTML = "";
-}
-function OpenIMGG()
-{
-    document.getElementById("ExtraInfoCG").innerHTML = "<p>Pan echo con productos 100% organicos.</p><h5>Precio unitario:</h5><p>$0.25</p><h5>Precio al por mayor:</h5><p>$0.15</p>";
-    document.getElementById("ExtraInfoCA").innerHTML = "";
-    document.getElementById("ExtraInfoCB").innerHTML = "";
-    document.getElementById("ExtraInfoCC").innerHTML = "";
-    document.getElementById("ExtraInfoCD").innerHTML = "";
-    document.getElementById("ExtraInfoCE").innerHTML = "";
-    document.getElementById("ExtraInfoCF").innerHTML = "";
-    document.getElementById("ExtraInfoCH").innerHTML = "";
-    document.getElementById("ExtraInfoCI").innerHTML = "";
-    document.getElementById("ExtraInfoCJ").innerHTML = "";
-}
-function OpenIMGH()
-{
-    document.getElementById("ExtraInfoCH").innerHTML = "<p>Pan echo con productos 100% organicos.</p><h5>Precio unitario:</h5><p>$0.25</p><h5>Precio al por mayor:</h5><p>$0.15</p>";
-    document.getElementById("ExtraInfoCA").innerHTML = "";
-    document.getElementById("ExtraInfoCB").innerHTML = "";
-    document.getElementById("ExtraInfoCC").innerHTML = "";
-    document.getElementById("ExtraInfoCD").innerHTML = "";
-    document.getElementById("ExtraInfoCE").innerHTML = "";
-    document.getElementById("ExtraInfoCF").innerHTML = "";
-    document.getElementById("ExtraInfoCG").innerHTML = "";
-    document.getElementById("ExtraInfoCI").innerHTML = "";
-    document.getElementById("ExtraInfoCJ").innerHTML = "";
-}
-function OpenIMGI()
-{
-    document.getElementById("ExtraInfoCI").innerHTML = "<p>Pan echo con productos 100% organicos.</p><h5>Precio unitario:</h5><p>$0.25</p><h5>Precio al por mayor:</h5><p>$0.15</p>";
-    document.getElementById("ExtraInfoCA").innerHTML = "";
-    document.getElementById("ExtraInfoCB").innerHTML = "";
-    document.getElementById("ExtraInfoCC").innerHTML = "";
-    document.getElementById("ExtraInfoCD").innerHTML = "";
-    document.getElementById("ExtraInfoCE").innerHTML = "";
-    document.getElementById("ExtraInfoCF").innerHTML = "";
-    document.getElementById("ExtraInfoCG").innerHTML = "";
-    document.getElementById("ExtraInfoCH").innerHTML = "";
-    document.getElementById("ExtraInfoCJ").innerHTML = "";
-}
-function OpenIMGJ()
-{
-    document.getElementById("ExtraInfoCJ").innerHTML = "<p>Pan echo con productos 100% organicos.</p><h5>Precio unitario:</h5><p>$0.25</p><h5>Precio al por mayor:</h5><p>$0.15</p>";
-    document.getElementById("ExtraInfoCA").innerHTML = "";
-    document.getElementById("ExtraInfoCB").innerHTML = "";
-    document.getElementById("ExtraInfoCC").innerHTML = "";
-    document.getElementById("ExtraInfoCD").innerHTML = "";
-    document.getElementById("ExtraInfoCE").innerHTML = "";
-    document.getElementById("ExtraInfoCF").innerHTML = "";
-    document.getElementById("ExtraInfoCG").innerHTML = "";
-    document.getElementById("ExtraInfoCH").innerHTML = "";
-    document.getElementById("ExtraInfoCI").innerHTML = "";
+function Validar(){
+    let Nombre,Direccion,Municipio,Telefono,TermAccept;
+    Nombre = document.getElementById("Nombre");
+    if(Nombre.value != "")
+    {
+        document.getElementById("NombreVacio").innerHTML= "";
+        Direccion = document.getElementById("Direccion");
+        if(Direccion.value != "")
+        {
+            document.getElementById("DireccionVacia").innerHTML = "";
+            Municipio = document.getElementById("Municipio");
+            if(Municipio.value != "")
+            {   
+                document.getElementById("MunicipioVacio").innerHTML = "";
+                Telefono = document.getElementById("NumeroTel");
+                if(Telefono.value != "")
+                {   
+                    document.getElementById("TelefonoVacio").innerHTML = "";
+                    TermAccept = document.getElementById("Si").checked;
+                   if(CpNuevosCx.IAdquision.value != "")
+                   {
+                    document.getElementById("MotCompraVacio").innerHTML = "";
+                    if(TermAccept == true){
+                        document.getElementById("TermsVacio").innerHTML = "";
+                        alert("Sus datos han sido guardados con exito");
+                    }
+                    else{
+                        document.getElementById("TermsVacio").innerHTML = "Debes aceptar los terminos y condiciones para continuar";
+                        return false;
+                    }                   
+                   }
+                   else
+                   {
+                    document.getElementById("MotCompraVacio").innerHTML = "Este campo no puede estar vacio.";
+                    return false;
+                   }
+                }
+                else
+                {
+                    document.getElementById("TelefonoVacio").innerHTML = "Este campo no puede estar vacio.";
+                    Telefono.focus();
+                    return false;    
+                }
+            }
+            else
+            {
+                document.getElementById("MunicipioVacio").innerHTML = "Este campo no puede estar vacio.";
+                Municipio.focus();
+                return false;
+            }
+        }
+        else
+        {
+            document.getElementById("DireccionVacia").innerHTML = "Este campo no puede estar vacio.";
+            Direccion.focus();
+            return false;
+        }
+    }
+    else
+    {   
+        document.getElementById("NombreVacio").innerHTML = "Este campo no puede estar vacio.";
+        Nombre.focus();
+        return false;
+    }
 }
